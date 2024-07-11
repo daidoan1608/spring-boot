0912c606ba7e0225bcfccde2fe5c973fbf5e9c2a
Đoàn Minh Đại <daidoan1608@gmail.com>
2024-07-12 01:19:16 +0700

    Update

---
+++ src/main/java/com/vti/blogapp/service/PostServiceIpl.java
@@ -0,0 +1,61 @@
+package com.vti.blogapp.service;
+
+import com.vti.blogapp.dto.PostDto;
+import com.vti.blogapp.entity.Post;
+import com.vti.blogapp.form.PostCreateForm;
+import com.vti.blogapp.form.PostUpdateForm;
+import com.vti.blogapp.mapper.PostMapper;
+import com.vti.blogapp.repository.PostRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class PostServiceIpl implements PostService{
+    private PostRepository postRepository;
+
+    @Override
+    public List<PostDto> findAll() {
+        var posts = postRepository.findAll();
+        var dtos = new ArrayList<PostDto>();
+        for (Post post : posts) {
+            var dto = PostMapper.map(post);
+            dtos.add(dto);
+        }
+        return dtos;
+    }
+
+    @Override
+    public PostDto findById(Long id) {
+        return postRepository.findById(id)
+                .map(PostMapper::map)
+                .orElse(null);
+    }
+
+    @Override
+    public PostDto create(PostCreateForm form) {
+        var post = PostMapper.map(form);
+        var savedPost = postRepository.save(post);
+        return PostMapper.map(savedPost);
+    }
+
+    @Override
+    public PostDto update(Long id,PostUpdateForm form) {
+        var optional = postRepository.findById(id);
+        if (optional.isEmpty()) {
+            return null;
+        }
+        var post = optional.get();
+        PostMapper.map(form, post);
+        var savedPost = postRepository.save(post);
+        return PostMapper.map(savedPost);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        postRepository.deleteById(id);
+    }
+}
